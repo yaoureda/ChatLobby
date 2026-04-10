3989aa31ac73bbd6960ebc21619c1a009165b33c
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-04-10 15:38:43 +0200

    initialize spring boot project

---
+++ src/main/java/com/personal/chatlobby/ChatLobbyApplication.java
@@ -0,0 +1,13 @@
+package com.personal.chatlobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatLobbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatLobbyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personal/chatlobby/ChatLobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.chatlobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatLobbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
