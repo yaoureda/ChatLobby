a8418af2a3ca46f1e1a0b2580dce9aff8ee45d3f
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-04-10 19:02:21 +0200

    define basic Message entity

---
+++ src/main/java/com/personal/chatlobby/Message.java
@@ -0,0 +1,48 @@
+package com.personal.chatlobby;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Sender is required")
+    private String sender;
+
+    @NotBlank(message = "Content is required")
+    @Size(min = 1, message = "Content must be at least 3 characters")
+    private String content;
+
+    public Message() {
+    }
+
+    public Message(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
