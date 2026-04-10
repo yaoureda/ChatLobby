33efa0d19beb87845a677e9cbe76c21f38028c45
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-04-10 20:54:16 +0200

    assign a timestamp to message if it does'nt have one

--- src/main/java/com/personal/chatlobby/MessageService.java
+++ src/main/java/com/personal/chatlobby/MessageService.java
@@ -2,6 +2,7 @@ package com.personal.chatlobby;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -18,6 +19,9 @@ public class MessageService {
     }
 
     public Message addMessage(Message message) {
+        if (message.getCreatedAt() == null) {
+            message.setCreatedAt(LocalDateTime.now());
+        }
         return messageRepository.save(message);
     }
 
