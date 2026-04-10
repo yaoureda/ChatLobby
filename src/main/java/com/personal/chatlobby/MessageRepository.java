289b9cf0d6a810eaaa52abf48cda08c79dcd9d41
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-04-10 19:14:03 +0200

    add the repository linked to database

---
+++ src/main/java/com/personal/chatlobby/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.personal.chatlobby;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findBySender(String sender);
+}
