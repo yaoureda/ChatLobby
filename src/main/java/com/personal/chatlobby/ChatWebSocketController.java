7e018ac92de373cd363b0e6a85c2053193e69223
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-04-10 21:46:16 +0200

    feat: make WebSocket destinations dynamic by room

--- src/main/java/com/personal/chatlobby/ChatWebSocketController.java
+++ src/main/java/com/personal/chatlobby/ChatWebSocketController.java
@@ -1,22 +1,34 @@
 package com.personal.chatlobby;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatWebSocketController {
 
     private final MessageService messageService;
+    private final SimpMessagingTemplate messagingTemplate;
 
-    public ChatWebSocketController(MessageService messageService) {
+    public ChatWebSocketController(MessageService messageService,
+                                   SimpMessagingTemplate messagingTemplate) {
         this.messageService = messageService;
+        this.messagingTemplate = messagingTemplate;
     }
 
     @MessageMapping("/chat.send")
-    @SendTo("/topic/messages")
-    public Message sendMessage(ChatMessage chatMessage) {
-        Message message = new Message(chatMessage.getSender(), chatMessage.getContent());
-        return messageService.addMessage(message);
+    public void sendMessage(ChatMessage chatMessage) {
+        Message message = new Message(
+                chatMessage.getSender(),
+                chatMessage.getContent(),
+                chatMessage.getRoom()
+        );
+
+        Message savedMessage = messageService.addMessage(message);
+
+        messagingTemplate.convertAndSend(
+                "/topic/rooms/" + savedMessage.getRoom(),
+                savedMessage
+        );
     }
 }
