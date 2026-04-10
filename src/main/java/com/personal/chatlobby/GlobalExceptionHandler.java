de730a7704a9d305c47428446793f502da559673
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-04-10 19:18:50 +0200

    feat: handle exception across controllers

---
+++ src/main/java/com/personal/chatlobby/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.personal.chatlobby;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return errors;
+    }
+}
