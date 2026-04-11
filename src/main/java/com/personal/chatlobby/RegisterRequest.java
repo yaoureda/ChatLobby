4bac3b657eddd00e49f496aa52cf680a84b3110f
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-04-11 12:44:42 +0200

    allow clients to send registering requests

---
+++ src/main/java/com/personal/chatlobby/RegisterRequest.java
@@ -0,0 +1,34 @@
+package com.personal.chatlobby;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 30)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 100)
+    private String password;
+
+    public RegisterRequest() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
