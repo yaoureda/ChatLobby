2553e2accbcc660de84d582da813bedadcd11331
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-04-23 16:10:25 +0200

    redirect to login page for '/' path

--- src/main/java/com/personal/chatlobby/controller/PageController.java
+++ src/main/java/com/personal/chatlobby/controller/PageController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/login";
+    }
+
     @GetMapping("/login")
     public String loginPage() {
         return "pages/login";
